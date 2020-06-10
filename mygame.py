ae129e5df58b48b45dd9abb1cf035c90fa7a805b
Optimus <optimusx51@hotmail.com>
2020-06-10 08:27:37 -0700

    Added play prompt & check

--- mygame.py
+++ mygame.py
@@ -1,5 +1,14 @@
 print('Welcome to the game!')
 name = input('Your name please')
-age = input('And you age?')
+age = int(input('And you age?'))
 
 print('Hello {} of age {}.'.format(name, age))
+
+if age >= 18:
+	print('You may pass..')
+
+	wants_to_play = input('Want to play a game? ')
+	if wants_to_play == 'yes':
+		print("Then let's play")
+else:
+	print('You may not pass.')
