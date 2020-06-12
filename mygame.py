891195af01b1089d066b5b4b2961751760d8eedb
Optimus <optimusx51@hotmail.com>
2020-06-11 22:52:21 -0700

    lower cased play input

--- mygame.py
+++ mygame.py
@@ -7,7 +7,7 @@ print('Hello {} of age {}.'.format(name, age))
 if age >= 18:
 	print('You may pass..')
 
-	wants_to_play = input('Want to play a game? ')
+	wants_to_play = input('Want to play a game? ').lower()
 	if wants_to_play == 'yes':
 		print("Then let's play")
 else:
