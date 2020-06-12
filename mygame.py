cacf6214b44db0e7eb4afb53d2008c5d982a3b30
Optimus <optimusx51@hotmail.com>
2020-06-11 23:40:48 -0700

    added story branches

--- mygame.py
+++ mygame.py
@@ -1,10 +1,8 @@
 print('Welcome to the game!')
-name = input('Your name please')
-age = int(input('And you age?'))
+name = input('Your name please: ')
+age = int(input('And you age? '))
 
 hp = 10
-
-print('Hello {} of age {}.'.format(name, age))
 
 if age >= 18:
 	print('You may pass..')
@@ -12,15 +10,35 @@ if age >= 18:
 	wants_to_play = input('Want to play a game? ').lower()
 	if wants_to_play == 'yes':
 		print("Then let's play")
+		print('Hello {} of age {}.  You have {} health.'.format(name, age, hp))
 
 		left_or_right = input('We begin...which direction do we go?\n(left or right?)').lower()
 		if left_or_right == 'left':
 			ans = input('You come upon a lake... Do you swim across or go around?/(swim/around)').lower()
-			if and == 'around':
-				print()
+			if ans == 'around':
+				print('You walk around the lake and come upon a house.')
+			elif ans == 'swim':
+				print('You see a mermaid as you swim across.  She bids you follow her and you do.  You swim after her down, down, down.')
+				while True:
+					hp -= 5
+					print('hp -5\nremaining hp = {}'.format(hp))
+					ans = input('Swim back to the surface or continue to follow?\n(up/down)')
+					if ans == 'down':
+						continue
+					else:
+						break
+				print('You swim back to the surface and make it to shore')
+				ans = input('You are at a delta.  There\'s a house nearby.  Do you go up the river or to the house?\n(river/house)')
+				if ans == 'house':
+					print('You go to the house.  A crazed man comes out running at you and bites you.  You manage to get him off and knock him out, but lose 5 hp.')
+					hp -= 5
+					print('Your hp is now at {} hp.'.format(hp))
+					if hp <= 0:
+						print('You are out of hp.  You have perished.  Game Over')
+				else:
+					print('The mermaid was waiting for you in ambush.  She grabs you by the arm and pulls you underwater...')
 			else:
-				print('You see a mermaid as you swim across.  She bids you follow her and you do.  You swim down, down, down, until you run out of breath.')
-
+				print('Poor choice...')
 		else:
 			print('You walked into a spider\'s web never to be seen again...')
 
